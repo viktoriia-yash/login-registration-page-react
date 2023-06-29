1ac0cd411d9226f387cda5fbed5c7efa7ee4b015
viktoriia-yash <yashkina.vic@gmail.com>
2023-06-30 00:06:43 +0300

    HTML code for Register page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <>
+      <div className="page">
+        <div className="row-one">
+          <h1></h1>
+        </div>
+        <div className="row-two">
+          <form>
+            <label for="username">Username</label>
+            <input name="username" id="username" placeholder="Your username" />
+            <label for="email">Email</label>
+            <input
+              name="email"
+              id="email"
+              placeholder="Your email"
+              type="email"
+            />
+            <label for="password">Password</label>
+            <input
+              name="password"
+              id="password"
+              placeholder="*******"
+              type="password"
+            />
+            <button>Join</button>
+          </form>
+          <button>Already have an account? Login</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Register
+export default Register;
