5f00c7994bc9b072e9beb5d9247019af72265eee
viktoriia-yash <yashkina.vic@gmail.com>
2023-06-29 23:31:09 +0300

    Basic HTML code for Login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <div className="page">
+        <div className="row-one">
+          <h1></h1>
+        </div>
+        <div className="row-two">
+          <form>
+            <label for="email">Email</label>
+            <input
+              type="email"
+              placeholder="Your email"
+              id="email"
+              name="email"
+            />
+            <label for="password">Password</label>
+            <input
+              type="password"
+              placeholder="*******"
+              id="password"
+              name="password"
+            />
+            <button>Login</button>
+          </form>
+          <button>New here? Register</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
